1da028184dea0312fca3a06a55cd4e4472f48353
Rafael <rafaasigoli1@gmail.com>
2025-01-09 17:08:08 -0300

    feat: add repositories

---
+++ src/main/java/com/blacktech/api/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.blacktech.api.repositories;
+
+import com.blacktech.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
